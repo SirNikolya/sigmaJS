f6e3207d6ab145fd985a2e70206666940708d52d
Николай <sirnikolya228@gmail.com>
2025-12-20 18:43:00 +0300

    functionArrow2

--- Lesson 4/index.js
+++ Lesson 4/index.js
@@ -17,18 +17,30 @@
 // user.sayHiStandart();
 // user.sayHiArrow();
 
-const formatSize = (bytes) => {
-    if (bytes < 1024) {
-        return `${bytes} B`;
+// const formatSize = (bytes) => {
+//     if (bytes < 1024) {
+//         return `${bytes} B`;
+//     }
+
+//     const mbytes = (bytes / 1024 / 1024).toFixed(2);
+
+//     return `${mbytes} MB`;
+// };
+
+// const filesSize = [500, 5000000, 500000];
+
+// for (const fileSize of filesSize) {
+//     console.log(formatSize(fileSize));
+// }
+
+const usdToRub = (usd) => {
+    if (usd > 300) {
+        return usd * 85;
+    } else if (usd > 1000) {
+        return usd * 90;
     }
 
-    const mbytes = (bytes / 1024 / 1024).toFixed(2);
-
-    return `${mbytes} MB`;
-};
-
-const filesSize = [500, 5000000, 500000];
-
-for (const fileSize of filesSize) {
-    console.log(formatSize(fileSize));
+    return usd * 80;
 }
+
+console.log(usdToRub(301))
