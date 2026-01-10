a0335daeece6df766959c21c22c8e7639fe2e0f8
Николай <sirnikolya228@gmail.com>
2026-01-10 20:42:52 +0300

    change

--- Lesson 2/index.js
+++ Lesson 2/index.js
@@ -1,10 +1,16 @@
 let totalPrice = 5000;
-const accountBalance = 4000;
+let accountBalance = 4900;
 let isVip = false;
-if (isVip === true)
-    console.log("У вас есть ВИП статус, вам предоставлена скидка в 10%")
-    totalPrice = totalPrice * 0.9;
-if (accountBalance >= totalPrice)
-    console.log("Успешно! Покупка сделана")
-else 
-    console.log("Недостаточно средств");
+
+if (isVip === true) {
+  console.log("У вас есть ВИП статус, вам предоставлена скидка в 10%");
+  totalPrice = totalPrice * 0.9;
+} else {
+  console.log("У вас нет ВИП статуса, вам не предоставлена скидка в 10%");
+}
+
+if (accountBalance >= totalPrice) {
+  console.log("Успешно! Покупка сделана");
+} else {
+  console.log("Недостаточно средств");
+}
